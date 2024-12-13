b983f046e607b4b5b9a2666524e779f3e41304d3
Yasiru NIlan <nilanyasiru@gmail.com>
2024-12-13 09:19:22 +0530

    added pipeline

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,5 +1,15 @@
 import winston from "winston";
 
+/**
+ * Logger instance configured with Winston.
+ * Logs messages at the 'error' level to both the console and a file named 'error.log'.
+ *
+ * @constant {Object} logger - The Winston logger instance.
+ * @property {string} level - The logging level, set to 'error'.
+ * @property {Array} transports - The list of transports for logging.
+ * @property {winston.transports.Console} transports[0] - Transport for logging to the console.
+ * @property {winston.transports.File} transports[1] - Transport for logging to a file named 'error.log'.
+ */
 const logger = winston.createLogger({
   level: "error",
   transports: [
